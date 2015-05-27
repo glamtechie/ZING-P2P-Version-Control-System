b074f73cb7764f30f293e31fd796529e54aa0ffb
Purvi Desai <pdesai@cs.ucsd.edu>
2015-05-26 18:03:43 -0700

    return error codes for filesystem APIs

--- filesystem_apis.go
+++ filesystem_apis.go
@@ -11,62 +11,76 @@ const (
 	TEMP = "temp"
 )
 
-func zing_init(id int) {
+func zing_init(id int) error {
 	out, err := exec.Command("/bin/sh", "filesystem_scripts/zing_init.sh", strconv.Itoa(id)).Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
 
-func zing_pull(branch string) {
+func zing_pull(branch string) error {
 	out, err := exec.Command("/bin/sh", "filesystem_scripts/zing_pull.sh", branch).Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
 
-func zing_add(filename string) {
+func zing_add(filename string) error {
 	out, err := exec.Command("/bin/sh", "filesystem_scripts/zing_add.sh", filename).Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
 
-func zing_commit() {
+func zing_commit() error {
 	out, err := exec.Command("/bin/sh", "filesystem_scripts/zing_commit.sh").Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
 
-func zing_make_patch_for_push(branch string, patchname string) {
+func zing_make_patch_for_push(branch string, patchname string) error {
 	out, err := exec.Command("/bin/sh", "filesystem_scripts/zing_make_patch_for_push.sh", branch, patchname).Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
 
-func zing_delete_branch(branch string) {
+func zing_delete_branch(branch string) error {
 	out, err := exec.Command("git branch -D", branch).Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
 
 func zing_abort_push() {
 	//TODO: ?
 }
 
-func zing_process_push(patchname string) {
+func zing_process_push(patchname string) error {
 	out, err := exec.Command("/bin/sh", "filesystem_scripts/zing_process_push.sh", patchname).Output()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s\n", out)
+	return nil
 }
