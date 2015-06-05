ac27feec9415fc0a70bd768060233f500b46e47a
Purvi Desai <pdesai@cs.ucsd.edu>
2015-06-04 18:47:02 -0700

    git test minor fix

--- make_client.sh
+++ make_client.sh
@@ -2,4 +2,6 @@
 
 cat ~/.ssh/id_rsa.pub | ssh glamtechie@137.110.91.190 'cat >> ~/.ssh/authorized_keys'
 mkdir $1; cd $1;
+git init
 git remote add origin glamtechie@137.110.91.190:/Users/glamtechie/Documents/dum
+cd ..
