bea43d47f8b833c2f90585072e493206a797010b
Purvi Desai <pdesai@cs.ucsd.edu>
2015-06-11 17:12:56 -0700

    change file len script

---
+++ src/eval_script/change_file_len.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+i=$2
+no=$3
+echo "no is"
+echo $no
+
+pushd . &> /dev/null
+cd "$1"
+while [ $i -lt $no ]
+do
+    dd if=/dev/zero of=file_to-create-$i bs=2k count=$4
+    zing add file_to-create-$i
+
+    zing commit -m "done"
+
+    zing pull
+
+    time zing push
+    
+    i=$[$i+1]
+done
+
+popd &> /dev/null
+
